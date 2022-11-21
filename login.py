5476e2ce8caf8cd809e4de206a0a7d10e8dbff17
nikolovaleo <nikolovaleo@gmail.com>
2022-11-20 21:35:02 -0600

    Update login.py

--- login.py
+++ login.py
@@ -24,20 +24,22 @@ def get_user_password(path):
 
 def ssh_login():
 
-    ssh = paramiko.SSHClient()
-    ssh.load_system_host_keys()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('tty.sdf.org', username=username, password=password)
-
 
     host = "tty.sdf.org"
     username = "nikolov"
     password = "mHfYoQEg5iyStw"
 
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('tty.sdf.org', username=username, password=password)
 
-    ssh(host, cmd, user, password, timeout=30, bg_run=False)
+    
     ssh_in, ssh_out, ssh_err = ssh.exec_command('ss -ltn')
     print(ssh_in, ssh_out, ssh_err)
+
+
+
 
 ssh_login()
 
